b7b777d8aef32d7319d294320080c20db5a42b85
lucas_galo <lucas_d>
2021-04-15 18:53:36 -0300

    Adicionando métodos

--- src/main/java/org/openjfx/AtimospheraController.java
+++ src/main/java/org/openjfx/AtimospheraController.java
@@ -2,8 +2,22 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
 
-public class AtimospheraController {
+public class AtimospheraController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources){
+        // TODO Auto-generated method stub
+    }
+
+    public void CloseApp(ActionEvent event ){
+        Platform.exit();
+        System.exit(0);
+    }
 
     @FXML
     private void switchToAtimosphera() throws IOException {
